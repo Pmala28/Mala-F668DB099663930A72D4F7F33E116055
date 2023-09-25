29910f053db773973df47ce44aa03aa1ee566ca9
Pmala28 <144931027+Pmala28@users.noreply.github.com>
2023-09-25 11:02:38 -0700

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,7 @@
+
+def LinearSearchProduct(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
